1034ed285cb27b2a95c087cb95a616613e2b46fa
Luna Gagnon Lavoie <raccoon.luna.sama@gmail.com>
2021-09-26 23:00:11 -0400

    ajout nouveau projet

--- js/button-contact.js
+++ js/button-contact.js
@@ -1,5 +1,6 @@
 $("#hide-info").click(function() {
     $(".phone").addClass("xpdiv-1");
-    $(".mail").addClass("xpdiv-2");
-    $(".resume").addClass("xpdiv-3");
+    $(".git").addClass("xpdiv-2");
+    $(".mail").addClass("xpdiv-3");
+    $(".resume").addClass("xpdiv-4");
 })
--- js/slider.js
+++ js/slider.js
@@ -6,3 +6,4 @@ new Glide('.glide-1').mount();
 
 new Glide('.glide-2').mount();
 
+new Glide('.glide-3').mount();
