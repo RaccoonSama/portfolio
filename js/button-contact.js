28e72ad2075c3d2b0fc9126f669121d9ef4f559c
Luna Gagnon Lavoie <raccoon.luna.sama@gmail.com>
2024-03-26 23:49:38 -0400

    missing point

--- js/button-contact.js
+++ js/button-contact.js
@@ -3,4 +3,5 @@ $("#hide-info").click(function() {
     $(".git").addClass("xpdiv-2");
     $(".mail").addClass("xpdiv-3");
     $(".resume").addClass("xpdiv-4");
+    $(".ppx").addClass("xpdiv-5")
 })
