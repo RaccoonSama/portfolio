397a4f38e0b52ac234c345c2d170c4380c6cbc44
Luna Gagnon Lavoie <raccoon.luna.sama@gmail.com>
2024-03-26 23:39:35 -0400

    update !

--- js/language.js
+++ js/language.js
@@ -15,7 +15,7 @@ $('#fr').click(
 $('#en').click(
     function() {
         fr = false;
-        console.log(fr);
+        console.log($('#GTdesc'));
 
 
         $('#header-prp').text(lang.eng.about);
@@ -24,7 +24,25 @@ $('#en').click(
         $('#propos h2').text(lang.eng.aboutTitle);
         $('#propos p').text(lang.eng.aboutTxt);
         $('#exp h2').text(lang.eng.real);
+        // GT
+        $('#GTdesc').text(lang.eng.gtWiki);
+        // React
+        $('#reactSchool').text(lang.eng.gtWiki);
+        // Bakery
+        $('#bakeryTitle').text(lang.eng.aapTitle);
+        $('#bakerySite').text(lang.eng.aapDesc);
+        // Html test
+        $('#funHtmlTitle').text(lang.eng.randomTitle);
+        $('#funHtmlDesc').text(lang.eng.randomDesc);
+        // css test
+        $('#funCssTitle').text(lang.eng.randomCssTitle);
+        $('#funCssDesc').text(lang.eng.randomCssDesc);
+        // Gamejam
+        $('#gameDevTitle').text(lang.eng.gameDevTitle);
+        $('#gameDevDesc').text(lang.eng.gameDevDesc);
+        // other
         $('.site-link').text(lang.eng.site);
+        $('#site-link-extra').text(lang.eng.site2)
         $('#coord h1').text(lang.eng.contactTitle);
         $('#hide-info').text(lang.eng.button);
         $('#resume-a').text(lang.eng.cv);
@@ -42,12 +60,27 @@ const lang = {
         xp : "Experience",
         contact : "Contact-me",
         aboutTitle : "Young web devlopper, always learning",
-        aboutTxt : "Student in multimedia who likes to learn et code. I like Video Games, music, roleplay and animated series bust most likely web. Bilingual English-French, self-taught, smart, resourceful and motivated",
+        aboutTxt : "Front end developper who likes to learn and code. I love js and anything related to it !. Bilingual English-French, self-taught, smart, resourceful and motivated",
         real : "Realisations",
         site : "Visit site",
+        site2 : "See other project",
+        // gt
+        gtWiki: "Wiki of an animated serie on youtube, creation of templates and modification of extensions. The website is sadly down as of today so please refer to the picture or wayback machine.",
+        // react
         reactTitle : "React prototype",
-        wpTitle : "Main page wordpress",
-        randomTitle : " fun main page of a website",
+        reactDesc: "React website prototypes, implementation tests with databases",
+        // bakery
+        aapTitle : "Audrey-Anna bakery website",
+        aapDesc : "Design and incorporation of templates in odoo for the website of Audrey-Anne Patisserie",
+        // fun html
+        randomTitle : "fun main page of a website",
+        randomDesc : "POC of a home page design as well as a style switcher in js",
+        // fun css
+        randomCssTitle: "Kirby css",
+        randomCssDesc: "Kirby entirely made in css, small personal test made for fun",
+        // gamedev
+        gameDevTitle: "Game Dev !",
+        gameDevDesc: "GameJam where we had to create a game under 48h, other link is a procedural dungeon map generator made by me for fun",
         contactTitle : "Contact-me",
         button : "see infos",
         cv : "Download my resume"
